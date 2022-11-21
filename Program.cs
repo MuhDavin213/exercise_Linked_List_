d7be6b653ba50ddf0b347fb94f4e4dbea8802c7c
MuhDavin213 <davin.new58@gmail.com>
2022-11-21 15:52:48 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_3
+{
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public string next;
+    }
+    class CircularList
+    {
+        Node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
